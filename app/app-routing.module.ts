3b62edde87051e02653b5770f719a4500f8a6167
Panzerchong <panzerchong@gmail.com>
2023-02-28 16:40:19 -0800

    page

--- app/app-routing.module.ts
+++ app/app-routing.module.ts
@@ -3,28 +3,14 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
-    path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
-  },
-  {
     path: '',
-    redirectTo: 'home',
-    pathMatch: 'full'
-  },  {
-    path: 'over-night-sleep',
-    loadChildren: () => import('./over-night-sleep/over-night-sleep.module').then( m => m.OverNightSleepPageModule)
-  },
-  {
-    path: 'sleepness-log',
-    loadChildren: () => import('./sleepness-log/sleepness-log.module').then( m => m.SleepnessLogPageModule)
-  },
-
+    loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+  }
 ];
-
 @NgModule({
   imports: [
     RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
   ],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
