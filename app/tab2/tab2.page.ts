6f3d832a6511ef099bdff621e3d36b05d0edcf07
Zhongri Jin <zhongrij@uci.edu>
2023-03-01 18:40:50 -0800

    ddd

--- app/tab2/tab2.page.ts
+++ app/tab2/tab2.page.ts
@@ -10,17 +10,56 @@ export class Tab2Page {
 
   showPicker = false;
   formattedString = '';
-  dateValue = format(new Date(),'yyyy-MM-dd') + 'T09:00:00.000Z';
+  formattedBedTime = '';
+  formattedEndDay = '';
+  formattedWakeUp = '';
+  dateValue = format(new Date(),'yyyy-MM-dd');
   constructor() {
     this.setToday();
+    this.setBedTime();
+    this.setEndDay();
+    this.setWakeUp();
+    
   }
 
   setToday(){
-    this.formattedString = format(parseISO(format(new Date(),'yyyy-MM-dd') + 'T09:00:00.000Z'), 'MMM d, yyyy');
+    this.formattedString = format(parseISO(format(new Date(),'yyyy-MM-dd')), 'MMM d, yyyy');
+  }
+  setBedTime(){
+    this.formattedBedTime = format(parseISO(format(new Date(),'yyyy-MM-dd')), 'HH:mm aaa');
+  }
+  setEndDay(){
+    this.formattedEndDay = format(parseISO(format(new Date(),'yyyy-MM-dd')), 'MMM d, yyyy');
+  }
+  setWakeUp(){
+    this.formattedWakeUp = format(parseISO(format(new Date(),'yyyy-MM-dd')), 'HH:mm aaa');
   }
   dateChanged(value: any){
     this.dateValue = value;
     this.formattedString = format(parseISO(value),'MMM d, yyyy');
-    this.showPicker = false;
+    console.log(this.formattedString)
+    this.showPicker = true; 
+  }
+  
+  timeChanged(ele: any){
+    this.dateValue = ele;
+    this.formattedBedTime = format(parseISO(ele),'HH:mm aaa');
+    this.showPicker = true;
+    console.log(this.formattedBedTime)
+  
+  }
+  endDateChanged(value: any){
+    this.dateValue = value;
+    this.formattedEndDay = format(parseISO(value),'MMM d, yyyy');
+    console.log(this.formattedEndDay)
+    this.showPicker = true; 
+  }
+  wakeUpTimeChanged(ele: any){
+    this.dateValue = ele;
+    this.formattedWakeUp = format(parseISO(ele),'HH:mm aaa');
+    this.showPicker = true;
+    console.log(this.formattedWakeUp)
+  
   }
 }
+
