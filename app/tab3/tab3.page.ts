3e6358fee3363d8427b8cc2748cc58d90a60cae5
Panzerchong <panzerchong@gmail.com>
2023-03-01 22:14:43 -0800

    wed

--- app/tab3/tab3.page.ts
+++ app/tab3/tab3.page.ts
@@ -7,21 +7,53 @@ import { Component } from '@angular/core';
 })
 export class Tab3Page {
 
-  options=[
-    {"content": "the first one",
-    "level": "1"},
-    {"content": "second one",
-    "level": "2"}
-  ];
-
-
-  constructor() {}
+  sleepiness:string="";
+  sleepinessLevel:number=0;
 
   enterComment: string="";
   comment: string="";
 
+  storeLevel:number=0;
+  storeDetail:string="";
+
+
+
+
+
+  options=[
+    {"content": "Feeling active and vital",
+    "level": "1"},
+    {"content": "Functioning at a high level, but not at peak; able to concentrate",
+    "level": "2"},
+    {"content": "Relaxed; awake; not at full alertness; responsive",
+    "level": "3"},
+    {"content": "A little foggy; not at peak; let down",
+    "level": "4"},
+    {"content": "Fogginess; beginning to lose interest in remaining awake; slowed down",
+    "level": "5"},
+    {"content": "Sleepiness; prefer to be lying down; fighting sleep; woozy",
+    "level": "6"},
+    {"content": "Almost in reverie; sleep onset soon; lost struggle to remain awake",
+    "level": "7"}
+  ];
+
+  
+
+  constructor() {}
+
+
   onClick(){
     this.comment=this.enterComment;
+  }
+
+  recordSleepiness(){
+
+
+    // this.store=this.sleepiness;
+    // console.log(item);
+    // this.sleepiness=item.content;
+
+    
   }
 
 }
