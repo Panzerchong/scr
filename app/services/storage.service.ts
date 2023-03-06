c44ee7010202033a007b76970b6dede868c34ff2
Panzerchong <panzerchong@gmail.com>
2023-03-05 19:35:00 -0800

    Update storage.service.ts

--- app/services/storage.service.ts
+++ app/services/storage.service.ts
@@ -4,9 +4,59 @@ import { StanfordSleepinessData } from '../../app/data/stanford-sleepiness-data'
 import { OvernightSleepData } from '../data/overnight-sleep-data';
 import { Storage } from '@ionic/storage-angular';
 
+
+const STORAGE_KEY='mylist'
+
 @Injectable({
   providedIn: 'root'
 })
 export class StorageService {
+  public static sleepinessRecord:StanfordSleepinessData[] = [];
+
+  constructor(private storage: Storage) {
+    this.init();
+  }
+
+  init(){this.storage.create();}
   
+
+  getData(){
+    return this.storage.get(STORAGE_KEY)||[];
+  }
+
+  async addData(item){
+    const storedData=await this.storage.get(STORAGE_KEY)||[];
+    storedData.push(item);
+    return this.storage.set(STORAGE_KEY,storedData);
+  }
+
+  async removeItem(index){
+    const storedData=await this.storage.get(STORAGE_KEY)||[];
+    storedData.splice(index,1);
+    return this.storage.set(STORAGE_KEY,storedData);
+  }
+
+
+
+
+
+
+
+
+    // public logSleepinessData(sleepData:StanfordSleepinessData) {
+
+  
+    //   StorageService.sleepinessRecord.push(sleepData);
+    //   //Ionic Storage
+    //   this.sleepinessStorage.set(sleepData.id, sleepData);
+    // }
+    
+    // public getAllSleepinessDataFromStorage(){
+    //   StorageService.sleepinessRecord.forEach( (element) => {
+    //     this.sleepinessStorage.get(element.id).then( (value) => {
+    //       console.log(value);
+    //     });
+    //   });
+    // }
+    
 }
