2a351335ab34926f84d5659dfc9f86c80edc9477
Zhongri Jin <zhongrij@uci.edu>
2023-03-05 17:06:36 -0800

    d
    1

--- app/data/overnight-sleep-data.ts
+++ app/data/overnight-sleep-data.ts
@@ -22,6 +22,22 @@ export class OvernightSleepData extends SleepData {
 		// Convert to hours and minutes
 		return Math.floor(difference_ms / (1000*60*60)) + " hours, " + Math.floor(difference_ms / (1000*60) % 60) + " minutes.";
 	}
+	summaryDuration():number {
+		var sleepStart_ms = this.sleepStart.getTime();
+		var sleepEnd_ms = this.sleepEnd.getTime();
+
+		// Calculate the difference in milliseconds
+		var difference_ms = sleepEnd_ms - sleepStart_ms;
+		if(difference_ms < 0){
+			difference_ms = 0;
+		  }
+		// Convert to hours and minutes
+		var hour = Math.floor(difference_ms / (1000*60*60));
+		var minutes =  Math.floor(difference_ms / (1000*60) % 60);
+		var covertMinToHour = minutes/60;
+		var total = hour + covertMinToHour;
+		return Math.round(total*100) /100;
+	}
 
 	override dateString():string {
 		return "Night of " + this.sleepStart.toLocaleDateString('en-US', { weekday: 'long', month: 'long', day: 'numeric' });
--- app/tab1/tab1.page.ts
+++ app/tab1/tab1.page.ts
@@ -13,23 +13,36 @@ export class Tab1Page implements OnInit{
   @ViewChild('barChart') barChart;
 
   id:any;
-  bars: any;
+  lines: any;
   colorArray: any;
   data:any;
+  duration:any = [];
   
   count:number = 0;
   showSleepDataCards: boolean;
   showSleepinessDataCards: boolean;
   overnightSleepDataArray: OvernightSleepData[];
   sleepinessDataArray:StanfordSleepinessData[];
+  isModalOpen = false;
+  setOpen(isOpen: boolean) {
+    this.isModalOpen = isOpen;
+   
+  }
   constructor(private sleepService:SleepService) { }
   ngOnInit(){
     this.showSleepDataCards = true;
     this.overnightSleepDataArray = SleepService.AllOvernightData;
     this.sleepinessDataArray = SleepService.AllSleepinessData;
     this.showSleepinessDataCards = false;
+    //this.createBarChart();
+    this.defineChartData();
   }
-
+  ionViewDidEnter() 
+  {
+    
+     this.createBarChart();   
+     
+  }
 
   segmentChanged(ele:any){
     console.log('Segment changed', ele);
@@ -39,12 +52,60 @@ export class Tab1Page implements OnInit{
       console.log("show sleep data!");
       this.showSleepDataCards = true; 
       this.showSleepinessDataCards = false;
+      this.createBarChart();  
+      console.log("created chart");
+     
+
+      
     } else if (ele.detail.value=="Sleepiness"){
+      
+      this.createBarChart();  
+      
       console.log("show sleepiness data!");
       this.showSleepDataCards = false;
       this.showSleepinessDataCards = true;
     }
   }
+  createBarChart() {
+
+    console.log("created chart");
+    console.log("created chart");
+    if(this.lines !=null){
+      this.lines.clear();
+      this.lines.destroy();
+    }
+
+    this.lines = new Chart(this.barChart.nativeElement, {
+      type: 'line',
+      data: {
+        labels: ['1','2','3','4','5','6','7'],
+        datasets: [{
+          label: 'Sleeping Duration',
+          data: this.duration,
+          backgroundColor: 'rgb(38, 194, 129)', // array should have same number of elements as number of dataset
+          borderColor: 'rgb(38, 194, 129)',// array should have same number of elements as number of dataset
+          borderWidth: 1
+        }]
+      },
+    });
+  }
+  defineChartData() : void
+   {
+      let k : any;
+
+      for(k in this.overnightSleepDataArray)
+      {
+         //var tech  =      this.technologies.technologies[k];
+
+         let convert = this.duration.push(this.overnightSleepDataArray[k].summaryDuration());
+
+         console.log("duration " + this.overnightSleepDataArray[k].summaryDuration());
+         console.log("Duration array " + this.duration);
+      }
+     
+      //document.write(d.getTime() + " milliseconds since 1970/01/01");
+   }
+   
 }
  
     
